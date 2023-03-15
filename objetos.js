4c4456f7a09aab675acf12a13a96ff04df4668a9
BetancurLopez <edwinbetancurlopez@gmail.com>
2023-03-14 22:43:37 -0500

    objetos

---
+++ objetos.js
@@ -0,0 +1,24 @@
+//ejemplo
+
+const cat = {
+    'name': 'Whiskers',
+    'legs': 4,
+    'tails': 1,
+    'enemies': ['Water', 'Dogs']
+};
+
+const anotherObject = {
+    make: 'Ford',
+    5: 'five',
+    'model': 'focus'
+};
+
+
+/*Crea un objeto que represente a un perro llamado myDog que contenga las propiedades name (una cadena), legs, tails y friends.
+Puedes establecer estas propiedades del objeto a los valores que quieras, siempre y cuando name sea una cadena, legs y tails sean números, y friends sea un arreglo.*/
+const myDog = {
+    name: 'Fito',
+    legs: 4,
+    tails: 1,
+    friends: ['cat', 'bird', 'food'] 
+};
